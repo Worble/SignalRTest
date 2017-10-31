5c76ae89582971dab9129898e1b120dd7db03d46
Robert Clarke <Robert.Clarke@northgateplc.com>
2017-10-31 15:19:23 +0000

    init

---
+++ SignalRTest/Controllers/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Controllers
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
